03417e9740bd578c2ba633a4be0b41e74259804b
cheruab <cheruabmelkamu693@gmail.com>
2026-03-02 14:07:56 +0300

    switch to postgres

--- nestjs-carental/src/database/database.module.ts
+++ nestjs-carental/src/database/database.module.ts
@@ -5,16 +5,14 @@ import { Car } from '../components/cars/entities/car';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '3306'),
-      username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASS || '12345678',
-      database: process.env.DB_NAME || 'yourcar',
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
       entities: [Car],
       synchronize: true,
       autoLoadEntities: true,
-      logging: true,
+      ssl: {
+        rejectUnauthorized: false, // required for Render's PostgreSQL
+      },
     }),
   ],
   exports: [TypeOrmModule],
